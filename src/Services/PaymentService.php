198c9c1cddf49e68149c3139628480dab8e9eee8
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-10-28 11:57:20 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -223,13 +223,9 @@ class PaymentService
                     $requestData['order_status'] = trim($this->config->get('Novalnet.'. $requestData['payment_method'] .'_order_completion_status'));
                     $requestData['paid_amount'] = 0;
                 } elseif(in_array($requestData['tid_status'], ['85', '91', '98', '99'])) {
-                    
-                    $requestData['order_status'] = trim($this->config->get('Novalnet.novalnet_onhold_confirmation_status'));
+$requestData['order_status'] = (isset($requestData['forced_onhold']) && !empty($requestData['forced_onhold'])) ? trim($this->config->get('Novalnet.novalnet_forced_onhold_order_status')) : trim($this->config->get('Novalnet.novalnet_onhold_confirmation_status'));
                     $requestData['paid_amount'] = 0;
-                } elseif(isset($requestData['forced_onhold']) && !empty($requestData['forced_onhold'])) {
-					$requestData['order_status'] = trim($this->config->get('Novalnet.novalnet_forced_onhold_order_status'));
-                    $requestData['paid_amount'] = 0;
-				}
+                }
             else {
                     $requestData['order_status'] = trim($this->config->get('Novalnet.'. $requestData['payment_method'] .'_order_completion_status'));
                     $requestData['paid_amount'] = ($requestData['tid_status'] == '100') ? $requestData['amount'] : '0';
